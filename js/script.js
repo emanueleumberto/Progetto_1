5a3250cc0163541a643ceb93c1aeefaa56bd32c9
emanueleumberto <info@emanueleumberto.it>
2022-04-05 14:41:33 +0200

    primo commit da vscode

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let arr = ['ciao', 'abc', 'bar'];
+let newArr = [...arr];
+newArr.push('new');
+console.log(arr);
+console.log(newArr);
+
+function func(x, y, z) {
+    console.log('Sono nella Func ' + x + ' - ' + y + ' - ' + z);
+}
+
+func(...arr);
+
+let arrCopy = Object.assign([], arr);
+arrCopy.splice(1,1,'newAbc');
+console.log(arrCopy);
+console.log(arr);
